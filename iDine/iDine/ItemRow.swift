f9232970a016ed8d74fd2f569e8e5ee4455f23e5
mike tihonchik <angry.guy.soft@gmail.com>
2025-03-27 18:52:50 -0600

    add some row customizations

--- iDine/iDine/ItemRow.swift
+++ iDine/iDine/ItemRow.swift
@@ -9,11 +9,23 @@ import SwiftUI
 
 struct ItemRow: View {
     let item: MenuItem
+    let colors: [String: Color] = ["D": .purple, "G": .black, "N": .red, "S": .blue, "V": .green]
     var body: some View {
         HStack {
-            Image(item.thumbnailImage)
-            Text(item.name)
+            Image(item.thumbnailImage).clipShape(Circle()).overlay(Circle().stroke(.gray, lineWidth: 2))
+            Text(item.name).font(.headline)
             Text("$\(item.price)")
+            Spacer()
+            // we need to tell Swift that the string itself is the identifier for each item
+            ForEach(item.restrictions, id: \.self) { restriction in
+                Text(restriction)
+                    .font(.caption)
+                        .fontWeight(.black)
+                        .padding(5)
+                        .background(colors[restriction, default: .black])
+                        .clipShape(Circle())
+                        .foregroundStyle(.white)
+            }
         }
     }
 }
